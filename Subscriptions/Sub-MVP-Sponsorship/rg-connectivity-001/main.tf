1037ba1825f872762d4b7cdc0a98116ca6a53100
Martin Therkelsen <martin@racket.dk>
2022-06-12 13:33:35 +0200

    Added vNet to environment

--- Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-001/main.tf
+++ Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-001/main.tf
@@ -1,4 +1,27 @@
-resource "azurerm_resource_group" "resourcegroups" {
+resource "azurerm_resource_group" "resourcegroup" {
     name        = var.ResourceGroup
     location    = var.Location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet.vNetName
+  address_space       = var.vnet.address_space
+  location            = var.Location
+  resource_group_name = var.ResourceGroup
+
+  depends_on = [
+    azurerm_resource_group.resourcegroup
+  ]
+}
+
+resource "azurerm_subnet" "subnets" {
+  for_each = var.Subnets
+  name                 = each.value["name"]
+  resource_group_name  = var.ResourceGroup
+  virtual_network_name = var.vnet.vNetName
+  address_prefixes     = each.value["prefix"]
+  
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ] 
 }
