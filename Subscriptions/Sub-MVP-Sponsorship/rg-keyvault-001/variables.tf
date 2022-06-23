8d25d0920637162cd769081ed8f1e5e134dc3678
Martin Therkelsen <martin@racket.dk>
2022-06-23 17:09:29 +0200

    Added keyvault

---
+++ Subscriptions/Sub-MVP-Sponsorship/rg-keyvault-001/variables.tf
@@ -0,0 +1,10 @@
+variable "Location" {
+    type                        = string
+    default                     = "WestEurope"  
+}
+
+variable "ResourceGroup" {
+    type                        = string
+    default                     = "rg-keyvault-001"   
+}
+
