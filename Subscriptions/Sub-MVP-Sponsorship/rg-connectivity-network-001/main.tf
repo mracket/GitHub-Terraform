9a6279dac50a18e399fb3576c37bff8c24032af6
Martin Therkelsen <martin@racket.dk>
2022-07-02 00:44:16 +0200

    added data object for firewall subnet

--- Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-network-001/main.tf
+++ Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-network-001/main.tf
@@ -90,7 +90,10 @@ resource "azurerm_virtual_network_gateway_connection" "VPN-Connection" {
 
   shared_key = data.azurerm_key_vault_secret.VPNSharedSecret.value
 }
-
+data "azurerm_subnet" "AzureFirewallSubnet" {
+  name                = "AzureFirewallSubnet"
+  resource_group_name = azurerm_subnet.subnets.resource_group_name
+}
 resource "azurerm_public_ip" "public-ip-AzureFirewall" {
   name                = "pip-${var.AzureFirewallName}"
   location            = azurerm_resource_group.resourcegroup.location
@@ -108,7 +111,7 @@ resource "azurerm_firewall" "AzureFirewall" {
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = azurerm_subnet.subnets["AzureFirewallSubnet"].id
+    subnet_id            = data.azurerm_subnet.AzureFirewallSubnet.id
     public_ip_address_id = azurerm_public_ip.public-ip-AzureFirewall.id
   }
 }
