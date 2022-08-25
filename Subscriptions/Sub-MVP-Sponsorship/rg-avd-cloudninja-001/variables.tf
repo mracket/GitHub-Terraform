7a866bf94d9e20e35f7ca35108bb0adbe24ea600
Martin Therkelsen <martin@racket.dk>
2022-08-25 13:53:59 +0200

    Added session hosts to the solution (#16)

--- Subscriptions/Sub-MVP-Sponsorship/rg-avd-cloudninja-001/variables.tf
+++ Subscriptions/Sub-MVP-Sponsorship/rg-avd-cloudninja-001/variables.tf
@@ -2,6 +2,10 @@ variable "Location" {
     type                = string
     default             = "WestEurope"  
 }
+variable "avd_Location" {
+    type                = string
+    default             = "NorwayEast"  
+}
 
 variable "ResourceGroup" {
     type                = string
@@ -11,4 +15,31 @@ variable "ResourceGroup" {
 variable "FSLogixStorageAccount" {
     type                = string
     default             = "cloudninjafsl11072022"   
+}
+
+variable "NumberOfSessionHosts" {
+    type = number
+    default = 2
+}
+
+variable "vm_prefix" {
+    type = string
+    default = "avd-h1"
+}
+
+variable "avd_vnet" {
+    type = string
+    default = "vnet-avd-001"
+}
+variable "avd_vnet_resource_group" {
+    type = string
+    default = "rg-avd-network-001"
+}
+variable "avd_hostpool_subnet" {
+    type = string
+    default = "snet-avd-hostpool-001"
+}
+variable "avd_agent_location" {
+    type = string 
+    default = "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_06-15-2022.zip"    
 }
