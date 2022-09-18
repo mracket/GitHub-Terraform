d84c16ef795b6cffc62a4ec6dea67c79cba25a32
Martin Therkelsen <martin@racket.dk>
2022-09-18 12:44:48 +0200

    Added NSG to connectivity

--- Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-network-001/FirewallPolicy.tf
+++ Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-network-001/FirewallPolicy.tf
@@ -1,3 +1,4 @@
+/*
 data "azurerm_firewall_policy" "AzureFirewallPolicy" {
     name = "afwp-connectivity-001"
     resource_group_name = azurerm_resource_group.resourcegroup.name
@@ -162,3 +163,4 @@ resource "azurerm_firewall_policy_rule_collection_group" "FirewallOnPremisesRule
     }
   } 
 }
+*/
--- Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-network-001/main.tf
+++ Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-network-001/main.tf
@@ -63,6 +63,7 @@ resource "azurerm_public_ip" "VPN-PublicIP" {
   depends_on = [azurerm_virtual_network.vnet]
 }
 
+
 resource "azurerm_virtual_network_gateway" "VPN-Gateway" {
   name                = "vgw-connectivity-001"
   location            = azurerm_resource_group.resourcegroup.location
@@ -108,6 +109,8 @@ resource "azurerm_virtual_network_gateway_connection" "VPN-Connection" {
 
   shared_key = data.azurerm_key_vault_secret.VPNSharedSecret.value
 }
+
+
 resource "azurerm_route_table" "routes" {
   name                          = "rt-${azurerm_virtual_network.vnet.name}"
   location                      = azurerm_resource_group.resourcegroup.location
